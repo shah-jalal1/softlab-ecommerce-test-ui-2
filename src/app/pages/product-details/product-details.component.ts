f71cdddc33c47048295672f695d7d63123d74cf8
shah-jalal1 <shah.jalal0894@gmail.com>
2021-11-18 16:11:05 +0600

    product details

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,31 +1,26 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ProductService} from '../../services/product.service';
-import {UtilsService} from '../../services/utils.service';
+
 import {NgForm} from '@angular/forms';
 import {Cart} from '../../interfaces/cart';
-import {UserDataService} from '../../services/user-data.service';
+// import {UserDataService} from '../../services/user-data.service';
 import {UserService} from '../../services/user.service';
-import {ReloadService} from '../../services/reload.service';
+// import {ReloadService} from '../../services/reload.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {UiService} from 'src/app/services/ui.service';
 import {PriceData, Product} from '../../interfaces/product';
 import {CartService} from '../../services/cart.service';
-// import {ReviewControl} from '../../interfaces/review-control';
-// import {ReviewControlService} from '../../services/review-control.service';
+
 import {NgxSpinnerService} from 'ngx-spinner';
 import {Breadcrumb} from '../../interfaces/breadcrumb';
-import {StorageService} from 'src/app/services/storage.service';
- import {RecommendedProductData} from 'src/app/interfaces/recommended-products-data';
-import {ProductSubCategory} from 'src/app/interfaces/product-sub-category';
+
 import {WishlistSchema} from '../../interfaces/wishlist';
-// import {DiscussionService} from '../../services/discussion.service';
-// import {Discussion} from '../../interfaces/discussion';
+
 import {User} from '../../interfaces/user';
 import {Pagination} from '../../interfaces/pagination';
 import {PricePipe} from '../../shared/pipes/price.pipe';
 import {Subscription} from 'rxjs';
 import {MatRadioChange} from '@angular/material/radio';
-
 
 @Component({
   selector: 'app-product-details',
@@ -36,27 +31,20 @@ import {MatRadioChange} from '@angular/material/radio';
 export class ProductDetailsComponent implements OnInit, OnDestroy {
 
   // Subscription
-  subRoute: Subscription;
-  subDataOne: Subscription;
-  subDataTwo: Subscription;
-  subDataThree: Subscription;
-  subDataFour: Subscription;
-  subDataFive: Subscription;
-  subDataSix: Subscription;
+  subRoute ?: Subscription;
+  subDataOne ?: Subscription;
+  subDataTwo ?: Subscription;
+  subDataThree ?: Subscription;
+  subDataFour ?: Subscription;
+  subDataFive ?: Subscription;
+  subDataSix ?: Subscription;
 
   // All Product
-  productSlug: string = null;
-  relatedProducts: Product[] = [];
-  product?: Product;
-
-  // Reviews
-  // allReviews: ReviewControl[] = [];
-  // Discussions
-  // allDiscussions: Discussion[] = [];
-  discussionsCount = 0;
+  productSlug !: string | any;
+  product ?: Product;
 
   // Image Zoom & View Area
-  @ViewChild('zoomViewer', {static: true}) zoomViewer;
+  @ViewChild('zoomViewer', {static: true}) zoomViewer: any;
   image: any;
   zoomImage: any;
 
@@ -80,48 +68,35 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
 
   // Unit Type
   productPriceData: PriceData[] = [];
-  selectedPriceData: PriceData = null;
+  selectedPriceData ?: PriceData | undefined;
 
   // User
-  user: User = null;
+  user ?: User | undefined;
 
 
   constructor(
     private productService: ProductService,
-    private utilsService: UtilsService,
-    private userDataService: UserDataService,
+    // private userDataService: UserDataService,
     private userService: UserService,
-    private reloadService: ReloadService,
+    // private reloadService: ReloadService,
     private activatedRoute: ActivatedRoute,
     public uiService: UiService,
     private cartService: CartService,
-    // private reviewControlService: ReviewControlService,
     private spinner: NgxSpinnerService,
-    private storageService: StorageService,
     public router: Router,
-    // public discussionService: DiscussionService,
     public pricePipe: PricePipe,
-  ) {
-  }
+  ) { }
 
   ngOnInit(): void {
-
-    // this.zoomViewer.nativeElement.children[0].style.display = 'none';
 
     this.subRoute = this.activatedRoute.paramMap.subscribe(param => {
       this.productSlug = param.get('slug');
       this.getSingleProductBySlug();
     });
 
-    this.reloadService.refreshCart$.subscribe(() => {
-      this.getCartsItems();
-    });
-
-
-    // this.reloadService.refreshDiscussion$.subscribe(() => {
-    //   this.getAllDiscussionsByQuery();
+    // this.reloadService.refreshCart$.subscribe(() => {
+    //   this.getCartsItems();
     // });
-
   }
 
 
@@ -133,97 +108,34 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
     this.spinner.show();
     this.subDataOne = this.productService.getSingleProductBySlug(this.productSlug)
       .subscribe(res => {
-        console.log(res.data);
         this.spinner.hide();
         this.product = res.data;
-        this.productPriceData = this.product.prices;
+        console.log(this.product);
+        // this.productPriceData = this.product.prices;
 
-        // Recommended Product
-         let data: RecommendedProductData = this.storageService.getViewedProductData();
-        const subCategory = this.product.subCategory as ProductSubCategory;
-        if (data) {
-          if (!data.productIds.includes(this.product._id)) {
-            data.productIds.push(this.product._id);
-          }
-          if (!data.subCategoryIds.includes(subCategory._id)) {
-            data.subCategoryIds.push(subCategory._id);
-          }
-        } else {
-          data = {
-            productIds: [this.product._id],
-            subCategoryIds: [subCategory._id]
-          };
-        }
-        // this.storageService.storeViewedProductData(data);
         // Recommended Product
 
         if (this.product) {
-          if (this.productPriceData && this.productPriceData.length) {
-            this.selectedPriceData = this.productPriceData[0];
-          }
-          this.updateBreadcrumb();
+          // if (this.productPriceData && this.productPriceData.length) {
+          //   this.selectedPriceData = this.productPriceData[0];
+          // }
+          // this.updateBreadcrumb();
           this.setDefaultImage();
-          this.getRelatedProducts();
-          this.getCartsItems();
-          // this.getAllReviewsByQuery();
-          // this.getAllDiscussionsByQuery();
-          if (this.ifLoggedIn()) {
-            this.getLoggedInUserInfo();
-          }
+          // this.getCartsItems();
+
+          // if (this.ifLoggedIn()) {
+          //   this.getLoggedInUserInfo();
+          // }
         }
       }, error => {
         this.spinner.hide();
         console.log(error);
       });
   }
-
-  // private getAllReviewsByQuery() {
-  //   const query = {
-  //     product: this.product._id,
-  //     status: true
-  //   };
-  //   this.subDataFour = this.reviewControlService.getAllReviewsByQuery(null, null, query)
-  //     .subscribe(res => {
-  //       this.allReviews = res.data;
-  //     }, error => {
-  //       console.log(error);
-  //     });
-  // }
-
-  private getRelatedProducts() {
-    let data;
-    data = {
-      // @ts-ignore
-      category: this.product.category._id,
-      id: this.product._id,
-    };
-    if (this.product.subCategory) {
-      // @ts-ignore
-      data = {...data, ...{subCategory: this.product.subCategory._id}};
-    }
-    this.subDataFive = this.productService.getRelatedProducts(data)
-      .subscribe(res => {
-        this.relatedProducts = res.data;
-      }, error => {
-        console.log(error);
-      });
-  }
-
-
-  addItemToCartDB(data: Cart) {
-    this.userDataService.addItemToUserCart(data)
-      .subscribe(res => {
-        this.uiService.success(res.message);
-        this.reloadService.needRefreshCart$();
-      }, error => {
-        console.log(error);
-      });
-  }
-
   /**
    * IMAGE ZOOM & VIEW AREA
    */
-  public onMouseMove(e) {
+  public onMouseMove(e: { currentTarget: any; offsetX: any; offsetY: any; }) {
     if (window.innerWidth >= 1099) {
       const image = e.currentTarget;
       const offsetX = e.offsetX;
@@ -240,7 +152,7 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
     }
   }
 
-  public onMouseLeave(event) {
+  public onMouseLeave(event: any) {
     this.zoomViewer.nativeElement.children[0].style.display = 'none';
   }
 
@@ -250,12 +162,11 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
   }
 
   private setDefaultImage() {
-    // this.image = this.product.images !== null ? this.product.images[0].big : '/assets/images/junk/Nokia 3310.jpg';
-    this.image = this.product.images && this.product.images.length > 0 ? this.product.images[0] : '/assets/images/placeholder/test.png';
-    // this.zoomImage = this.product.images[0].big;
+    this.image = this.product?.images !== null ? this.product?.images[0].big : '/assets/images/junk/Nokia 3310.jpg';
+    this.image = this.product?.images && this.product.images.length > 0 ? this.product.images[0] : '/assets/images/placeholder/test.png';
+    this.zoomImage = this.product?.images[0].big;
     this.zoomImage = this.image;
   }
-
 
   /**
    * QUANTITY CONTROL
@@ -274,223 +185,25 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * CART FUNCTIONALITY
-   */
-
-  addToCart() {
-    const data: Cart = {
-      product: this.product?._id,
-      selectedQty: this.selectedQty,
-      priceId: this.selectedPriceData._id
-    };
-
-
-    if (this.userService.getUserStatus()) {
-      this.addItemToCartDB(data);
-    } else {
-      this.cartService.addCartItemToLocalStorage(data);
-      this.reloadService.needRefreshCart$();
-    }
-  }
-
-  // GET CARTS DATA
-  private getCartsItems() {
-    if (this.userService.getUserStatus()) {
-      this.getCartItemList();
-    } else {
-      this.getCarsItemFromLocal();
-    }
-
-  }
-
-  private getCartItemList() {
-    this.subDataTwo = this.cartService.getCartItemList()
-      .subscribe(res => {
-        this.carts = res.data;
-        // @ts-ignore
-        const existsOnCart = this.carts.find(item => item.product._id === this.product._id);
-        if (existsOnCart) {
-          this.existsInCart = true;
-          this.selectedQty = existsOnCart.selectedQty;
-          this.selectedPriceData = this.productPriceData.find(f => f._id === existsOnCart.priceId);
-        }
-      }, error => {
-        console.log(error);
-      });
-  }
-
-  private getCarsItemFromLocal() {
-    const items = this.cartService.getCartItemFromLocalStorage();
-
-    if (items && items.length > 0) {
-      const ids: string[] = items.map(m => m.product as string);
-      this.productService.getSpecificProductsById(ids, 'productName productSlug  price discountType discountAmount  quantity images')
-        .subscribe(res => {
-          const products = res.data;
-          if (products && products.length > 0) {
-            this.carts = items.map(t1 => ({...t1, ...{product: products.find(t2 => t2._id === t1.product)}}));
-            // @ts-ignore
-            const existsOnCart = this.carts.find(item => item.product._id === this.product._id);
-            if (existsOnCart) {
-              this.existsInCart = true;
-              this.selectedQty = existsOnCart.selectedQty;
-              this.selectedPriceData = this.productPriceData.find(f => f._id === existsOnCart.priceId);
-            }
-          }
-        });
-    } else {
-      this.carts = [];
-    }
-  }
-
-  private getLoggedInUserInfo() {
-    const select = 'phoneNo fullName profileImg email';
-    this.subDataSix = this.userDataService.getLoggedInUserInfo(select)
-      .subscribe(res => {
-        this.user = res.data;
-      }, error => {
-        console.log(error);
-      });
-  }
-
-
-  /**
-   * ON SUBMIT Review
-   */
-
-  onSubmit(formDirective: NgForm) {
-  }
-
-  /**
    * Breadcrumb CUSTOM
    */
-  private updateBreadcrumb() {
-    this.breadcrumbs = [
-      {
-        label: 'Home',
-        url: '/',
-        icon: 'fas fa-home'
-      },
-      {
-        label: this.product?.categorySlug,
-        url: `/product-list/${this.product.categorySlug}`
-      },
-      {
-        label: this.product?.subCategorySlug,
-        url: `/product-list/${this.product.categorySlug}/${this.product.subCategorySlug}`
-      },
-      {
-        label: this.product?.productName,
-        url: null
-      }
-    ];
-  }
-
-  /**
-   * HTTP REQ HANDLE AND ONCLICK CHECK AND CALL ADD TO WISHLIST
-   */
-
-  addItemToWishlistDB(data: WishlistSchema) {
-    this.userDataService.addSingleItemToWishlist(data)
-      .subscribe(res => {
-        this.uiService.success(res.message);
-      }, error => {
-        console.log(error);
-      });
-  }
-
-  ifLoggedIn() {
-    return this.userService.getUserStatus();
-  }
-
-  addToWishlistItemAfterCheck() {
-    this.userDataService.checkStatusInWishlistWithBookId(this.product._id)
-      .subscribe(res => {
-        // const count = res.data;
-        const exists = res.exists;
-
-        if (this.userService.getUserStatus()) {
-          // if (count === 0) {
-          if (!exists) {
-            const data: WishlistSchema = {
-              product: this.product?._id
-            };
-            this.addItemToWishlistDB(data);
-          } else {
-            // WARNING MESSAGE FOR REGISTERED USER THAT ITEM ALREADY EXISTS
-            this.uiService.warn(res.message);
-          }
-        }
-      }, error => {
-        console.log(error);
-      });
-  }
-
-
-  /**
-   * DISCUSSION AREA
-   */
-  /**
-   * DISCUSSION AREA
-   */
-  // private getAllDiscussionsByQuery() {
-  //   const query = {
-  //     product: this.product._id,
-  //     status: true
-  //   };
-  //   const pagination: Pagination = {
-  //     currentPage: '1',
-  //     pageSize: '10'
-  //   };
-  //   this.subDataThree = this.discussionService.getAllDiscussionsByQuery(pagination, null, query)
-  //     .subscribe(res => {
-  //       // this.allDiscussions = res.data;
-  //       this.discussionsCount = res.count;
-  //     }, error => {
-  //       console.log(error);
-  //     });
-  // }
-
-  // onSubmitDiscussion(event: any) {
-  //
-  //   const mData: Discussion = {
-  //     ...event,
-  //     ...{
-  //       product: this.product._id,
-  //       discussionDate: new Date(),
-  //       reply: [],
-  //       status: false
+  // private updateBreadcrumb() {
+  //   this.breadcrumbs = [
+  //     {
+  //       label: 'Home',
+  //       url: '/',
+  //       icon: 'fas fa-home'
+  //     },
+  //     {
+  //       label: this.product?.categorySlug,
+  //       url: `/product-list/${this.product.categorySlug}`
+  //     },
+  //     {
+  //       label: this.product?.productName,
+  //       url: `/${this.product.categorySlug}`
   //     }
-  //   };
-  //
-  //   this.addDiscussion(mData);
+  //   ];
   // }
-
-  // private addDiscussion(data: Discussion) {
-  //   this.discussionService.addDiscussion(data)
-  //     .subscribe(res => {
-  //       this.uiService.success(res.message);
-  //       this.reloadService.needRefreshDiscussion$();
-  //     }, error => {
-  //       console.log(error);
-  //     });
-  // }
-
-  /**
-   * ON Price Unit Data SELECT
-   */
-  onPriceDataSelect(event: MatRadioChange) {
-    this.selectedPriceData = event.value;
-  }
-
-  /**
-   * CALCULATE EMI VALUE
-   */
-
-  private calculateEmiValue(emiMonth: number) {
-    const productPrice = this.pricePipe.transform(this.product, 'priceWithDiscount');
-    return Math.floor(productPrice / emiMonth);
-  }
 
   ngOnDestroy() {
     if (this.subRoute) {
@@ -515,6 +228,4 @@ export class ProductDetailsComponent implements OnInit, OnDestroy {
       this.subDataSix.unsubscribe();
     }
   }
-
-
 }
